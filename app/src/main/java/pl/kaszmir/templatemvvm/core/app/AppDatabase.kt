2ea3bacd224775ba571d32ced7d594205579617c
krzysztof <krzysztof@pawscout.com>
2020-07-21 23:01:52 +0200

    Nie ma potrzeby implementowania tych metod, wystarczy zrobić klasę AppDatabase jako abstract i Room sam to sobie ogarnie :)

--- app/src/main/java/pl/kaszmir/templatemvvm/core/app/AppDatabase.kt
+++ app/src/main/java/pl/kaszmir/templatemvvm/core/app/AppDatabase.kt
@@ -1,24 +1,7 @@
 package pl.kaszmir.templatemvvm.core.app
 
-import androidx.room.DatabaseConfiguration
-import androidx.room.InvalidationTracker
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteOpenHelper
 
 const val databaseName = "templatemvvm-db"
 
-class AppDatabase : RoomDatabase() {
-
-    override fun createOpenHelper(config: DatabaseConfiguration?): SupportSQLiteOpenHelper {
-        TODO("Not yet implemented")
-    }
-
-    override fun createInvalidationTracker(): InvalidationTracker {
-        TODO("Not yet implemented")
-    }
-
-    override fun clearAllTables() {
-        TODO("Not yet implemented")
-    }
-
-}
+abstract class AppDatabase : RoomDatabase()
