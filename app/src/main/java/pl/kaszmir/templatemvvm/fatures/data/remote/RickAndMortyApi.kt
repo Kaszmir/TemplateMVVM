2cbe456d126784ddc3bf88f16874ac16bbc28f4f
krzysztof <krzysztof@pawscout.com>
2020-08-04 00:40:09 +0200

    added api documentation address to the RickAndMortyApi.kt file

--- app/src/main/java/pl/kaszmir/templatemvvm/fatures/data/remote/RickAndMortyApi.kt
+++ app/src/main/java/pl/kaszmir/templatemvvm/fatures/data/remote/RickAndMortyApi.kt
@@ -3,6 +3,7 @@ package pl.kaszmir.templatemvvm.fatures.data.remote
 
 /**
  * Interface dedicated to communication with backend
+ * https://rickandmortyapi.com/documentation/
  */
 
 interface RickAndMortyApi {
